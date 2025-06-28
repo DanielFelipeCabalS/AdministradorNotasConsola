ff2372c9944cc7c0cb92578ae9762f7688a1afa0
DanielFelipe <danielfelipecabalsanchez@gmail.com>
2025-06-28 00:01:13 -0500

    se agrego funciones de obtener indice, de imprimir(evita redundancia), se modifico editar nota para que se actualice con el indice e igual en eliminar nota

--- administrador_notas.py
+++ administrador_notas.py
@@ -14,6 +14,17 @@ def menu():
     6. Eliminar nota
     7. Salir''')
 
+def imprimir_nota(nota):
+    print(f"\nId: {nota['id']}"
+          f"\nTítulo: {nota['titulo']}"
+          f"\nContenido: {nota['contenido']}\n")
+
+def obtener_indice_id(id_nota):
+    for i, nota in enumerate(notas):
+        if nota['id'] == id_nota:
+            return i
+    return None
+
 def agregar_nota():
     global id
     titulo = input('Ingrese el título de la nota: ')
@@ -24,17 +35,13 @@ def agregar_nota():
 
 def mostrar_notas():
     for nota in notas:
-        print(f'''\t\tId: {nota.get('id')}
-        Título: {nota.get('titulo')}
-        Contenido: {nota.get('contenido')}''')
+        imprimir_nota(nota)
 
 def buscar_nota_id():
     buscar = int(input('Ingrese el id de la nota: '))
     for nota in notas:
         if nota.get('id') == buscar:
-            print(f'''\t\t\tId: {nota.get('id')}
-            Título: {nota.get('titulo')}
-            Contenido: {nota.get('contenido')}''')
+            imprimir_nota(nota)
             return nota
     print('Id no encontrado')
 
@@ -43,9 +50,7 @@ def buscar_nota_palabra():
     contador = 0
     for nota in notas:
         if buscar in nota.get('titulo') or buscar in nota.get('contenido'):
-            print(f'''\t\t\tId: {nota.get('id')}
-            Título: {nota.get('titulo')}
-            Contenido: {nota.get('contenido')}''')
+            imprimir_nota(nota)
             contador += 1
     if contador > 0:
         return
@@ -56,44 +61,40 @@ def editar_nota():
     nota = None
     nota = buscar_nota_id()
     if nota != None:
+        indice = obtener_indice_id(nota['id'])
         print('1. Si deseas modificar el título'
               '\n2. Si deseas modificar el contenido'
               '\n3. Si deseas modificar todo')
         op = int(input('Ingrese la opción a modificar: '))
         if op == 1:
             titulo = input('Ingrese el nuevo título: ')
-            notas [nota.get('id')-1] = {'id': nota.get('id'), 'titulo': titulo, 'contenido': nota.get('contenido')}
+            notas [indice] = {'id': nota.get('id'), 'titulo': titulo, 'contenido': nota.get('contenido')}
             print('Título modificado')
-            print(f'''\t\t\tId: {notas[nota.get('id')-1].get('id')}
-            Título: {notas[nota.get('id')-1].get('titulo')}
-            Contenido: {notas[nota.get('id')-1].get('contenido')}''')
+            imprimir_nota(notas[indice])
         elif op == 2:
             contenido = input('Ingrese el nuevo contenido de la nota: ')
-            notas[nota.get('id') - 1] = {'id': nota.get('id'), 'titulo': nota.get('titulo'), 'contenido': contenido}
+            notas[indice] = {'id': nota.get('id'), 'titulo': nota.get('titulo'), 'contenido': contenido}
             print('Contenido modificado')
-            print(f'''\t\t\tId: {notas[nota.get('id') - 1].get('id')}
-            Título: {notas[nota.get('id') - 1].get('titulo')}
-            Contenido: {notas[nota.get('id') - 1].get('contenido')}''')
+            imprimir_nota(notas[indice])
         elif op == 3:
             titulo = input('Ingrese el nuevo título: ')
             contenido = input('Ingrese el nuevo contenido de la nota: ')
-            notas[nota.get('id') - 1] = {'id': nota.get('id'), 'titulo': titulo, 'contenido': contenido}
-            print(f'''\t\t\tId: {notas[nota.get('id') - 1].get('id')}
-            Título: {notas[nota.get('id') - 1].get('titulo')}
-            Contenido: {notas[nota.get('id') - 1].get('contenido')}''')
+            notas[indice] = {'id': nota.get('id'), 'titulo': titulo, 'contenido': contenido}
+            imprimir_nota(notas[indice])
         else:
             print('Ingresaste una opción inválida')
             editar_nota()
 
 def eliminar_nota():
     mostrar_notas()
-    eliminar = int(input('\nIngrese el id de la nota a eliminar: '))
-    for nota in notas:
-        if eliminar == nota.get('id'):
-            notas.pop(eliminar - 1)
-        else:
-            print('Ingrese un id válido.')
-            eliminar_nota()
+    eliminar = int(input('Ingrese el id de la nota a eliminar: '))
+    for i, nota in enumerate(notas):
+        if eliminar == nota['id']:
+            del notas[i]
+            print('Nota eliminada\n')
+            return
+    print('Ingrese un id válido.\n')
+    eliminar_nota()
 
 if __name__ == '__main__':
     while True:
